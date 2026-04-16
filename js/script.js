48de286d4d7f74219296210b0aed52d38cbd293e
mchlsry <michaelsurya796@gmail.com>
2026-04-17 01:30:37 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('.typing-text', {
+        strings: [
+            'UI/UX Designer.', 
+            'Web Designer.', 
+            'Web Developer.', 
+            'Front End Developer.'
+        ],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 1500,
+        loop: true,
+        cursorChar: '|',
+    });
+});
